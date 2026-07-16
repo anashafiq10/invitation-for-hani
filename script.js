277fadce940c0af2e0ee0f901b6b9527a743ed83
anashafiq10 <mfa6164@psu.edu>
2026-07-16 16:02:20 -0400

    Update message content in script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const invite = {
   dressCode: "Wear whatever makes you feel cute (psstt cream beige if possible)",
   sideNote:
     "Hellowwww, sayang. Yes, a walk (sowwyy), i know you not a big fan, buttttttt trust me, we walk together ( I carry you but a little bit of walking kenala hehe). Btw, You can park your car at open air Central Market, cari yg bawah pokok",
-  message: "I want to take you somewhere sweet, spend real time with you, and make the whole evening feel easy, see youu there love",
+  message: "I want to  spend real time with you, and make the whole day feel amayzingto, see youu there love",
   mapLink: "https://maps.app.goo.gl/6b7P5htrYd5jTDTbA",
 };
 
